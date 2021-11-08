606cb472d0dfe65f86afb0a626979b6c8bef1a4a
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-08 18:42:14 +0100

    data clase con los modelos que vamos a recoger del json

---
+++ app/src/main/java/ut02/exercise2_alerts/domain/AlertModel.kt
@@ -0,0 +1,10 @@
+package ut02.exercise2_alerts.domain
+
+data class AlertModel(
+    val alert_id: String,
+    val title: String,
+    val summary: String,
+    val type: String,
+    val date: String,
+
+)
