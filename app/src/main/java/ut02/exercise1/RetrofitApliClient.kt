3d78c19faa8d8055bfd20aadb3bf75153231b7b7
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-08 10:40:37 +0100

    Uso de la libreria de retrofit

--- app/src/main/java/ut02/exercise1/RetrofitApliClient.kt
+++ app/src/main/java/ut02/exercise1/RetrofitApliClient.kt
@@ -1,11 +1,55 @@
 package ut02.exercise1
 
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitApliClient:ApiClient {
-    override fun getUser(userId: Int): UserApiModel? {
-        TODO("Not yet implemented")
+    private val urlEndPoint: String = "https://jsonplaceholder.typicode.com/"
+    private var apiEndPoint:ApiEndPoint
+
+    init {
+        apiEndPoint = buildApiService()
     }
 
-    override fun getUsers(): List<UserApiModel> {
-        TODO("Not yet implemented")
+    private fun buildApiService(): ApiEndPoint {
+        return buildClient().create(ApiEndPoint::class.java)
     }
+
+
+    private fun buildClient(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(urlEndPoint)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+
+    override fun getUsers(): List<UserApiModel> {
+        val call = apiEndPoint.getUsers()
+        val response = call.execute()
+        if (response.isSuccessful) {
+            val users = response.body()
+            //Si tiene usuarios los devuelvo, sino, devuelvo un listado vacío.
+            return users ?: mutableListOf()
+        } else {
+            return mutableListOf()
+        }
+    }
+
+
+    override fun getUser(userId: Int): UserApiModel? {
+        val call = apiEndPoint.getUser(userId)
+        val response = call.execute()
+        return if (response.isSuccessful) {
+            response.body()
+        } else {
+            null
+        }
+    }
+
+
+
 }
